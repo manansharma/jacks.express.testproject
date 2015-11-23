4d4fc05ec7bc55d863d6aaf8daaef5b02e1f6404
Manan Sharma <masharma@cigital.com>
2015-11-23 17:00:53 +0530

    second giudance

--- src/express-xpoweredby/index.js
+++ src/express-xpoweredby/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+//function customHeaders( req, res, next ){
+//  // Switch off the default 'X-Powered-By: Express' header
+//  app.disable( 'x-powered-by' );
+//
+//  // OR set your own header here
+//  res.setHeader( 'X-Powered-By', 'Awesome App v0.0.1' );
+//
+//  // .. other headers here
+//
+//  next()
+//}
+//
+//app.use( customHeaders );

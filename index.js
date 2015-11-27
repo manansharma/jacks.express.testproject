9f4ccde90be135a54ed8141697fbbdc283e028be
Manan Sharma <masharma@cigital.com>
2015-11-28 02:29:07 +0530

    add tests

--- index.js
+++ index.js
@@ -3,53 +3,13 @@ var session = require('express-session');
 var csrf = require('csurf');
 
 var app = express();
-app.disable('X-Powered-By');
+
+// ************** for random number test ********
+require('./src/javascript core/clientside prng/test.negative-randomnumber-use')(app, session);
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-// express-x-powered-by
   console.log('Example app listening at http://%s:%s', host, port);
-    
-    
 });
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-    
-    // Verify URL before redirecting
-  res.redirect('redirecting to http://www.codiscope.com');
-});
-
-
-// enable secure session
-// enable httponly session
-app.use(session({
-  secret: 'My super session secret',
-  cookie: {
-//    httpOnly: true,
-//    secure: true
-  }
-}));
-
-app.use(csrf());
-
-app.use(function(req, res, next) {
-  res.locals._csrf = req.csrfToken();
-  next();
-});
-
-//function customHeaders( req, res, next ){
-//  // Switch off the default 'X-Powered-By: Express' header
-//  app.disable( 'x-powered-by' );
-//
-//  // OR set your own header here
-//  res.setHeader( 'X-Powered-By', 'Awesome App v0.0.1' );
-//
-//  // .. other headers here
-//
-//  next()
-//}
-//
-//app.use( customHeaders );
---
+++ src/javascript core/clientside prng/test.negative-randomnumber-use.js
@@ -0,0 +1,16 @@
+module.exports = function(app, session) {
+    
+  app.use(session({
+      secret: 'session secret',
+      cookie: {
+        httpOnly: true,
+        secure: true
+      }
+    }));
+  
+  app.get('/explicitly_using_randomnumber', function(req, res) {
+    var mySess = req.session;
+      var rnd = Math.random();
+    res.send('Please check your cookie' + rnd);
+  });
+};

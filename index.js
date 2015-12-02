89499bb033061de72eafbcb27202e0c9d21577da
Manan Sharma <masharma@cigital.com>
2015-12-02 18:26:53 +0530

    ..

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ app.use(helmet());
 // ************** for Express - enable secure session tests ********
 //require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
 //require('./src/express/EnableSecureSession/test.securesessionfalse')(app, session);
-require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
+//require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
 //require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);
+//
+//// ************** for Express - open redirect tests ********
+require('./src/express/open-redirect/test.valid-open-redirect')(app, session);
 //
 //// ************** for random number tests ********
 //require('./src/javascript core/clientside prng/test.negative-randomnumber-use')(app, session);
+
+
 
 
 var server = app.listen(3000, function () {

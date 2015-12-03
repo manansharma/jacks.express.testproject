3829b36e2b224a227f819f341745f6d868e16f58
unknown <Nayan.Kathuria@NDI-LAP-365.3pillar.corp>
2015-12-03 13:38:53 +0530

    ..

--- src/node-and-node-modules/test.invalid-insecure-ssl-protocol-globalagent.js
+++ src/node-and-node-modules/test.invalid-insecure-ssl-protocol-globalagent.js
@@ -1,7 +1,7 @@
 var https = require('https');
 var fs = require('fs');
 
-https.globalAgent.options.secureProtocol = 'SSLv3_client_method';
+https.globalAgent.options.secureProtocol = 'SSLv2_method';
 var options = {
   key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
   cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')

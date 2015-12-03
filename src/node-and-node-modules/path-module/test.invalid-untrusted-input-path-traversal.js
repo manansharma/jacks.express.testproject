a2741aeb1e7e4dd2fbf7ca44dc6b560b2d79fa5f
unknown <Nayan.Kathuria@NDI-LAP-365.3pillar.corp>
2015-12-03 15:37:15 +0530

    ..

--- src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal.js
+++ src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var app = require('express')();
+
 
 // Start the app and visit the following URLs
 // Path Injection - http://localhost:3000/..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2Fetc
@@ -14,22 +14,9 @@ app.get('/path/:username', function(req, res) {
     res.send('Your home directory is located at: ' + getUserHomeDirectory(username));
 });
 
-app.get('/safepath/:username', function(req, res) {
+app.get('/path/:username', function(req, res) {
     var username = req.params.username;
-
-    // Valid username contains Alpha-Numeric characters, is at least 3 characters long but less than 16
-    // var re = /^[A-Za-z0-9]{3,16}$/g;
-
-    //if (re.test(username)) {
-    //    res.send('Your home directory is located at: ' + getUserHomeDirectory(username));
-    //} else {
-    //    res.send('Invalid username');
-    //}
-});
-
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Path Injection listening at http://%s:%s', host, port);
+	
+	//Valid username contains Alpha-Numeric characters, is at least 3 characters long but less than 16
+    res.send('Your home directory is located at: ' + getUserHomeDirectory(username));
 });

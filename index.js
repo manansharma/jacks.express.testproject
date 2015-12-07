127c3821cadc6432ef46bdd4727a81810b2ceee1
Manan Sharma <masharma@cigital.com>
2015-12-07 15:35:21 +0530

    ..

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.set("views", process.cwd() + "/views");
 
 // ************** for Express - enable http only session tests ********
 //require('./src/express/Enable HttpOnly Session/test.valid-enableHttpOnlySession')(app, session);
-require('./src/express/Enable HttpOnly Session/test.httponlyfalse')(app, session);
-//require('./src/express/Enable HttpOnly Session/test.missing-httponly')(app, session);
+//require('./src/express/Enable HttpOnly Session/test.httponlyfalse')(app, session);
+require('./src/express/Enable HttpOnly Session/test.missing-httponly')(app, session);
 //require('./src/express/Enable HttpOnly Session/test.setting-httponly-outside')(app, session);
 //
 // ************** for Express - enable secure session tests ********

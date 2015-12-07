67acd26b1e54e1a4b0fa352156ad15166e966daf
Manan Sharma <masharma@cigital.com>
2015-12-07 14:40:52 +0530

    ..

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.use(helmet());
 app.set("view engine", "html");
 app.set("views", __dirname + "/views");
 
-require('./src/HTML/test.invalid-insecure-script.html');
-//require('./src/HTML/test.invalid-insecure-script.js')(app, session);
+//require('./src/HTML/test.invalid-insecure-script.html');
+require('./src/HTML/test.invalid-insecure-script.')(app, session);
 
 
 // ************** for Express - enable http only session tests ********

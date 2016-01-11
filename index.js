87246aec3f84825b2a7926f20000ccba57ce581e
unknown <Nayan.Kathuria@NDI-LAP-365.3pillar.corp>
2016-01-11 16:28:10 +0530

    index.js updated

--- index.js
+++ index.js
@@ -23,44 +23,44 @@ app.set("view engine", "html");
 app.set("views", process.cwd() + "/views");
 //app.set("views", __dirname + "/views");
 
-//require('./src/HTML/test.invalid-insecure-script.html');
-//require('./src/HTML/test.invalid-insecure-script')(app);
+require('./src/HTML/test.invalid-insecure-script.html');
+require('./src/HTML/test.invalid-insecure-script')(app);
 
 
 // ************** for Express - enable http only session tests ********
-//require('./src/express/Enable HttpOnly Session/test.valid-enableHttpOnlySession')(app, session);
-//require('./src/express/Enable HttpOnly Session/test.httponlyfalse')(app, session);
-//require('./src/express/Enable HttpOnly Session/test.missing-httponly')(app, session);
-//require('./src/express/Enable HttpOnly Session/test.setting-httponly-outside')(app, session);
+require('./src/express/Enable HttpOnly Session/test.valid-enableHttpOnlySession')(app, session);
+require('./src/express/Enable HttpOnly Session/test.httponlyfalse')(app, session);
+require('./src/express/Enable HttpOnly Session/test.missing-httponly')(app, session);
+require('./src/express/Enable HttpOnly Session/test.setting-httponly-outside')(app, session);
 //
 // ************** for Express - enable secure session tests ********
-//require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
-//require('./src/express/EnableSecureSession/test.securesessionfalse')(app, session);
-//require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
-//require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);
+require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
+require('./src/express/EnableSecureSession/test.securesessionfalse')(app, session);
+require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
+require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);
 //
 //// ************** for Express - open redirect tests ********
-//require('./src/express/open-redirect/test.valid-open-redirect')(app, session);
+require('./src/express/open-redirect/test.valid-open-redirect')(app, session);
 //
 //// ************** for random number tests ********
-//require('./src/javascript core/clientside prng/test.negative-randomnumber-use')(app, session);
+require('./src/javascript core/clientside prng/test.negative-randomnumber-use')(app, session);
 //
 //// ************** for mongodb insert tests ********
-//require('./src/mongodb/insert/test.invalid-untrusted-input-insert')(app, session);
+require('./src/mongodb/insert/test.invalid-untrusted-input-insert')(app, session);
 //
 //// ************** for mongoDB find tests ********
-//require('./src/mongodb/find/test.find-with-untrusted-input')(app, session);
+require('./src/mongodb/find/test.find-with-untrusted-input')(app, session);
 //
 //// ************** for serverside injection tests ********
-//require('./src/javascript core/server-side-injection/test.invalid-serverside-injection')(app, session);
+require('./src/javascript core/server-side-injection/test.invalid-serverside-injection')(app, session);
 //
 //// ************** for node and node modules - https module injection tests ********
-//require('./src/node-and-node-modules/https-module/test.invalid-insecure-ssl-protocol-globalagent')(app, session);
-//require('./src/node-and-node-modules/https-module/test.invalid-insecure-ssl-protocol-httpsoption')(app, session);
+require('./src/node-and-node-modules/https-module/test.invalid-insecure-ssl-protocol-globalagent')(app, session);
+require('./src/node-and-node-modules/https-module/test.invalid-insecure-ssl-protocol-httpsoption')(app, session);
 require('./src/node-and-node-modules/https-module/test.invalid-insecure-ssl-protocol-requestagentoptions')(app, session);
 //
 //// ************** for node and node modules - path module injection tests ********
-//require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
+require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
 
 
 

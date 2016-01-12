1cc3a8b5944bd3844b77d78631487e2f050946a1
unknown <Nayan.Kathuria@NDI-LAP-365.3pillar.corp>
2016-01-12 11:58:39 +0530

    index.js updated

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
 //require('./src/express/EnableSecureSession/test.securesessionfalse')(app, session);
 //require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
-require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);
+//require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);
 //
 //// ************** for Express - open redirect tests ********
 //require('./src/express/open-redirect/test.valid-open-redirect')(app, session);
@@ -63,7 +63,9 @@ require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(ap
 //// ************** for node and node modules - path module injection tests ********
 //require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
 //require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal_1')(app, session);
-
+//
+//// ************** for JavaScript Core - cross site scripting ********
+require('./src/javascript core/cross-site-scripting/test.invalid-crossside-scripting')(app, session);
 
 
 var server = app.listen(3000, function () {

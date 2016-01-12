c6159dd33af0c64eed9b1f3429629705a69346be
unknown <Nayan.Kathuria@NDI-LAP-365.3pillar.corp>
2016-01-12 11:10:36 +0530

    index.js updated

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 //require('./src/node-and-node-modules/https-module/test.invalid-insecure-ssl-protocol-requestagentoptions')(app, session);
 //
 //// ************** for node and node modules - path module injection tests ********
-//require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
-require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal_1')(app, session);
+require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
+//require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal_1')(app, session);
 
 
 

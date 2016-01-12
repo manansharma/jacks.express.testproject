2a09475c71a56dba6db2bad4a8d60dbb1b2aec9a
unknown <Nayan.Kathuria@NDI-LAP-365.3pillar.corp>
2016-01-12 11:49:00 +0530

    index.js updated

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //require('./src/express/Enable HttpOnly Session/test.setting-httponly-outside')(app, session);
 //
 // ************** for Express - enable secure session tests ********
-//require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
+require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
 //require('./src/express/EnableSecureSession/test.securesessionfalse')(app, session);
 //require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
 //require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);
@@ -61,7 +61,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //require('./src/node-and-node-modules/https-module/test.invalid-insecure-ssl-protocol-requestagentoptions')(app, session);
 //
 //// ************** for node and node modules - path module injection tests ********
-require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
+//require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
 //require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal_1')(app, session);
 
 

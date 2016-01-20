9de828e01e5499a0ea6980c6ea0046035b42dc00
unknown <Kanchan.verma@NDI-LAP-327.3pillar.corp>
2016-01-20 15:31:30 +0530

    index.js updated

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 // ************** for Express - enable secure session tests ********
 //require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
 //require('./src/express/EnableSecureSession/test.securesessionfalse')(app, session);
-require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
-//require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);
+//require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
+require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);
 //
 //// ************** for Express - open redirect tests ********
 //require('./src/express/open-redirect/test.valid-open-redirect')(app, session);

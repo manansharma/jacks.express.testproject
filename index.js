9dc011ecd8109d19caf3f84c8ae015c349943f4e
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-01-21 18:18:30 +0530

    coment all

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ app.set("views", __dirname + "/src/views");
 //require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal_1')(app, session);
 //
 //// ************** for JavaScript Core - cross site scripting ********
-app.engine('html', ejs.renderFile);
-require('./src/javascript core/cross-site-scripting/test.invalid-crossside-scripting')(app, session);
+//app.engine('html', ejs.renderFile);
+//require('./src/javascript core/cross-site-scripting/test.invalid-crossside-scripting')(app, session);
 
 
 var server = app.listen(3000, function () {

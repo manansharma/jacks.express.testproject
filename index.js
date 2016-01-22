c0d62f79588b563759bda30cf0b85ddede5c80b7
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-01-22 14:52:37 +0530

    test

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.set("views", __dirname + "/src/views");
 //require('./src/express/Enable HttpOnly Session/test.valid-enableHttpOnlySession')(app, session);
 require('./src/express/Enable HttpOnly Session/test.httponlyfalse')(app, session);
 require('./src/express/Enable HttpOnly Session/test.missing-httponly')(app, session);
-//require('./src/express/Enable HttpOnly Session/test.setting-httponly-outside')(app, session);
+require('./src/express/Enable HttpOnly Session/test.setting-httponly-outside')(app, session);
 //
 // ************** for Express - enable secure session tests ********
 //require('./src/express/EnableSecureSession/test.missing-secure')(app, session);

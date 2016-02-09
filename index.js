d6e132b32a56ff585a97c1c6c8bf1c70bb9bedb6
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-02-09 15:42:07 +0530

    revert

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.set("views", __dirname + "/src/views");
 //require('./src/express/Enable HttpOnly Session/test.setting-httponly-outside')(app, session);
 //
 // ************** for Express - enable secure session tests ********
-require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
+//require('./src/express/EnableSecureSession/test.missing-secure')(app, session);
 //require('./src/express/EnableSecureSession/test.securesessionfalse')(app, session);
 //require('./src/express/EnableSecureSession/test.setting-secure-outside')(app, session);
 //require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(app, session);

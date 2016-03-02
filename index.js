ca80c9a257f86f7cd29661e361931b65a6b8a431
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-02 13:16:53 +0530

    test

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ require('./src/express/EnableSecureSession/test.valid-enable-secure-session')(ap
 //require('./src/node-and-node-modules/https-module/test.invalid-insecure-ssl-protocol-requestagentoptions')(app, session);
 //
 //// ************** for node and node modules - path module injection tests ********
-//require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
-//require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal_1')(app, session);
+require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal')(app, session);
+require('./src/node-and-node-modules/path-module/test.invalid-untrusted-input-path-traversal_1')(app, session);
 //
 //// ************** for JavaScript Core - cross site scripting ********
 //app.engine('html', ejs.renderFile);
